264c3f48b3073130b066f476a4decc7dd155a1f3
l3libs <78123640+l3libs@users.noreply.github.com>
2021-01-28 11:25:42 +0700

    Update and rename diisi_2.js to diisi_1.js

---
+++ 3/diisi_1.js
@@ -0,0 +1,2 @@
+l3file("https://cdn.statically.io/gh/l3libs/l/main/3/css/diisi_1.css", "css")
+l3file("https://cdn.statically.io/gh/l3libs/l/main/3/js/diisi_1.js", "js")
--- 3/diisi_2.js
+++
@@ -1,2 +0,0 @@
-l3file("https://cdn.statically.io/gh/l3libs/l/main/3/css/diisi_1.css", "css")
-l3file("https://cdn.statically.io/gh/hamdaniazzah/hamdaniazzah.github.io/master/l3/js/diisi_1.js", "js")

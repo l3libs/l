64955f6581f735e00a23beccecc13c699ad16d12
l3libs <78123640+l3libs@users.noreply.github.com>
2021-01-28 11:52:48 +0700

    Update and rename post_lirik_2.js to post_lirik_1.js

--- 3/post_lirik_2.js
+++
@@ -1,3 +0,0 @@
-l3file("https://cdn.statically.io/gh/l3libs/l/main/3/css/post_lirik_2.css", "js")
-l3file("https://cdn.statically.io/gh/l3libs/l/main/3/js/post_lirik_3.js", "css")
-l3file("https://cdn.ampproject.org/v0/amp-bind-0.1.js", "js")
